cde536418ab5a4ffa2c191aa640e4b268ec358c6
IPGLA\marcos.brandolin <marcosbrandolin@gmail.com>
2016-12-12 16:25:09 -0300

    final

--- v6/vibrate.js
+++ v6/vibrate.js
@@ -6,9 +6,10 @@ function myTimer()
     {
        myVid.pause();
 	   			$("#example-four .button").vibrate("long"); 
+          console.long('aqui che');
 				
 							$("#example-four .button").vibrate("long"); 
-
+console.long('aqui che');
 
        window.clearInterval(timer);
     }
